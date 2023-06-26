14274139075c7bb2c30f57e94d4138c6f1bce50a
Janakiram A <ram@MINJA245.local>
2023-06-26 15:39:04 +0530

    fix(): api & google key fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCq6oYSEYgv06BLONDYujOaIskHVfeuXVc',
-    authDomain: 'library-management-system-lms.firebaseapp.com',
-    projectId: 'library-management-system-lms',
-    storageBucket: 'library-management-system-lms.appspot.com',
-    messagingSenderId: '1086641789904',
-    appId: '1:1086641789904:web:798b35829f4eba8e8637d5',
-    measurementId: 'G-TXLJ7LCT3X',
+    apiKey: 'AIzaSyCq6********',
+    authDomain: 'library-management-sys********eapp.com',
+    projectId: 'library-managem********-lms',
+    storageBucket: 'library-***********ot.com',
+    messagingSenderId: '1086*******04',
+    appId: '1**********ba8e8637d5',
+    measurementId: 'G-TX******T3X',
   },
+  googleClientId: '1086641789904****************',
 };
